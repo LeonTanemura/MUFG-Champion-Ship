a20c366588e6935fe16286cf4ea125b4c621fd17
LeonTanemura <leontanemura@gmail.com>
2024-08-27 12:41:20 +0900

    add: xgboostの追加

--- model/gbm.py
+++ model/gbm.py
@@ -1,9 +1,12 @@
 import lightgbm as lgb
 import xgboost as xgb
+from lightgbm.callback import log_evaluation, early_stopping
 from sklearn.utils.validation import check_X_y
+import numpy as np
+import pandas as pd
 
 from .base_model import BaseClassifier, BaseRegressor
-from .utils import f1_micro, f1_micro_lgb, binary_logloss
+from .utils import f1_micro, f1_micro_lgb, binary_logloss, qwk_obj, quadratic_weighted_kappa
 
 
 class XGBoostClassifier(BaseClassifier):
@@ -61,20 +64,30 @@ class XGBoostRegressor(BaseRegressor):
     def __init__(self, input_dim, output_dim, model_config, verbose, seed=None) -> None:
         super().__init__(input_dim, output_dim, model_config, verbose)
         self.model = xgb.XGBRegressor(
-            objective="reg:squarederror",  # 目的関数を回帰用に変更
-            eval_metric="rmse",             # 評価指標を回帰用に変更
-            early_stopping_rounds=50,
+            # objective="reg:squarederror",  # 目的関数を回帰用に変更
+            objective=qwk_obj,
             **self.model_config,
-            random_state=seed
+            random_state=seed,
         )
 
     def fit(self, X, y, eval_set):
         self._column_names = X.columns
         X, y = check_X_y(X, y)
-        eval_set = [(eval_set[0].values, eval_set[1])]  # eval_setを適切な形式に変更
+        eval_set = [(X_val.values if isinstance(X_val, pd.DataFrame) else X_val, y_val) for X_val, y_val in eval_set]
 
-        self.model.fit(X, y, eval_set=eval_set, verbose=self.verbose > 0)
-        # print("XGBoostモデルの使用されたパラメータ:", self.model.get_params())
+        xgb_callbacks = [
+            xgb.callback.EvaluationMonitor(period=10000),
+            xgb.callback.EarlyStopping(100, metric_name="QWK", maximize=True, save_best=True)
+        ]
+
+        self.model.fit(
+            X,
+            y,
+            eval_set=eval_set,
+            eval_metric=quadratic_weighted_kappa,
+            callbacks=xgb_callbacks,
+            verbose=0
+        )
 
     def feature_importance(self):
         return self.model.feature_importances_
@@ -84,8 +97,8 @@ class LightGBMRegressor(BaseRegressor):
         super().__init__(input_dim, output_dim, model_config, verbose)
 
         self.model = lgb.LGBMRegressor(
-            objective="regression",         # 目的関数を回帰用に変更
-            verbose=self.verbose,
+            objective=qwk_obj,
+            # objective='regression',
             random_state=seed,
             **self.model_config,
         )
@@ -93,16 +106,18 @@ class LightGBMRegressor(BaseRegressor):
     def fit(self, X, y, eval_set):
         self._column_names = X.columns
         X, y = check_X_y(X, y)
-        eval_set = [(eval_set[0].values, eval_set[1])]  # eval_setを適切な形式に変更
+
+        eval_set = [(X_val.values if isinstance(X_val, pd.DataFrame) else X_val, y_val) for X_val, y_val in eval_set]
 
         self.model.fit(
             X,
             y,
+            eval_names=['train', 'valid'],
             eval_set=eval_set,
-            eval_metric="rmse",                    # 評価指標を回帰用に変更
-            callbacks=[lgb.early_stopping(stopping_rounds=50, verbose=self.verbose > 0)],
+            eval_metric=quadratic_weighted_kappa, 
+            callbacks=[early_stopping(stopping_rounds=100)],
+            # callbacks=[log_evaluation(period=100), early_stopping(stopping_rounds=75)],
         )
-        # print("LightGBMモデルの使用されたパラメータ:", self.model.get_params())
         
     def feature_importance(self):
         return self.model.feature_importances_
