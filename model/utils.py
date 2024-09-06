a6ff4f7f199c1e870bc836bdba658917f80710e4
LeonTanemura <leontanemura@gmail.com>
2024-09-06 15:17:29 +0900

    fix: 提出用に変更

--- model/utils.py
+++ model/utils.py
@@ -17,35 +17,48 @@ def f1_micro_lgb(y_true, y_pred):
 def binary_logloss(y_true, y_pred):
     return "binary_logloss", log_loss(y_true, y_pred), False
 
+
+# 今回 a は未使用
 def quadratic_weighted_kappa(y_true, y_pred):
     a = 0
+    # y_pred が XGB の QuantileDMatrix インスタンスかどうかを確認
     if isinstance(y_pred, xgb.QuantileDMatrix):
-        # XGB
+        # XGB の場合
+        # y_true と y_pred を入れ替える
         y_true, y_pred = y_pred, y_true
-
+        # ラベルデータを取得し、四捨五入
         y_true = (y_true.get_label() + a).round()
+        # 予測値をクリップして四捨五入
         y_pred = (y_pred + a).clip(0, 4).round()
+        # QWKスコアを計算
         qwk = cohen_kappa_score(y_true, y_pred, weights="quadratic")
         return 'QWK', qwk
 
     else:
-        # For lgb
+        # LightGBM 用の処理
+        # ラベルに a を加算
         y_true = y_true + a
+        # 予測値をクリップして四捨五入
         y_pred = (y_pred + a).clip(0, 4).round()
+        # QWKスコアを計算
         qwk = cohen_kappa_score(y_true, y_pred, weights="quadratic")
         return 'QWK', qwk, True
 
 def qwk_obj(y_true, y_pred):
     a = 0
     b = 1
+    # ラベルと予測値に a を加算
     labels = y_true + a
     preds = y_pred + a
+    # 予測値をクリップ
     preds = preds.clip(0, 4)
+    # 損失関数 f と g を計算
     f = 1/2*np.sum((preds-labels)**2)
-    g = 1/2*np.sum((preds-a)**2+b)
+    g = 1/2*np.sum((preds-a)**2 + b)
+    # 勾配とヘッセを計算
     df = preds - labels
     dg = preds - a
-    grad = (df/g - f*dg/g**2)*len(labels)
+    grad = (df/g - f*dg/g**2) * len(labels)
     hess = np.ones(len(labels))
     return grad, hess
 
